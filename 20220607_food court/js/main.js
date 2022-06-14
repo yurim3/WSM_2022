90f775d96b957917edbc9fd4e01593d83b6b21b0
SSep <ssep4u@gmail.com>
2022-06-14 14:59:23 +0900

    js 여러가지 함수 모습 복습

---
+++ 20220607_food court/js/main.js
@@ -0,0 +1,10 @@
+// function say() {
+//     console.log('hello world');
+// }
+
+// const say = function() {
+//     console.log('hello world2');
+// }
+
+// const say = () => console.log('hello world3');
+// say();
