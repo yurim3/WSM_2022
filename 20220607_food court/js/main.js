27b12cb6ddc3a45999018b82ec39388d5af45af1
SSep <ssep4u@gmail.com>
2022-06-14 15:57:22 +0900

    js change toggle icon

--- 20220607_food court/js/main.js
+++ 20220607_food court/js/main.js
@@ -2,12 +2,17 @@
 const toggleMenu = (toggleId, navListId) => {
   const toggle = document.getElementById(toggleId);
   const navList = document.getElementById(navListId);
+  const toggleIcon = toggle.getElementsByTagName("i")[0];
 
   if (toggle && navList) {
     // add: 추가, remove: 제거, toggle: 추가/제거
-    toggle.addEventListener("click", () =>
-      navList.classList.toggle("show-menu")
-    );
+    toggle.addEventListener("click", () => {
+      //toggle menu
+      navList.classList.toggle("show-menu");
+      //change toggle icon: bx-menu <-> bx-x-circle
+      toggleIcon.classList.toggle("bx-menu");
+      toggleIcon.classList.toggle("bx-x-circle");
+    });
   }
 };
 toggleMenu("nav-toggle", "nav-list");
