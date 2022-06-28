5c85da20d0953fddcf3e225e098429726fe8b47f
SSep <ssep4u@gmail.com>
2022-06-28 15:48:51 +0900

    html,css,js addNow()

--- 20220607_food court/js/main.js
+++ 20220607_food court/js/main.js
@@ -27,3 +27,32 @@ toggleMenu("nav-toggle", "nav-list");
 
 // const say = () => console.log('hello world3');
 // say();
+const addNow = (homeCardId) => {
+  //html -> js
+  const homeCard = document.getElementById(homeCardId);
+
+  //시간 -> index번째 식사가 선택되어야하는지 결정
+  //조식 끝: 8:00
+  //중식 끝: 13:00
+  //석식 끝: 17:50
+  var now = new Date();
+  let hour = now.getHours();
+  let minute = now.getMinutes();
+  // console.log(hour, minute);  //14 30
+  minute = hour * 60 + minute;
+  if (minute >= 17 * 60 + 50) {  //1070
+    index = 0;
+  } else if (13 * 60 <= minute) {
+    index = 2;
+  } else if (8 * 60 <= minute) {
+    index = 1;
+  } else {
+    index = 0;
+  }
+  // console.log(index);
+
+  //homeCard에서 index번째 card에 now 클래스 추가
+  let card = homeCard.getElementsByClassName('card')[index];
+  card.classList.add('now');
+}
+addNow('home-card');
